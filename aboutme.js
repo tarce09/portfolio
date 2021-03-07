bcbb796c3abbb1a39c975bbfcd999d2f0e020215
tarce09 <aryanraju09@gmail.com>
2021-03-07 11:30:18 +0530

    first commit

---
+++ aboutme.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll",function(){
+    var headerr=document.querySelector('nav');
+    headerr.classList.toggle("sticky",window.scrollY>0);
+})
+var icon=document.querySelector(".footerlogo")
+icon.addEventListener("mouseover",function(){
+    icon.style.height="2.5rem";
+    icon.style.width="2.5rem";
+    icon.style.background.size="150%";
+})
+icon.addEventListener("mouseout",function(){
+    icon.style.height="2rem";
+    icon.style.width="2rem";
+})
